1f5025030c12f5c1e7d54bf1f22aa3cea1b3bae6
favour ololade <ololadefavourajayi@gmail.com>
2024-07-08 11:39:07 +0100

    fixing

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ import styles from '../styles/Header.module.css';
 const Header = () => (
   <header className={styles.header}>
     <Image src="/profile.jpg" alt="Profile Picture" width={150} height={150} />
-    <h1>My Portfolio</h1>
+    <h1 className={styles.headerTitle}>My Portfolio</h1>
   </header>
 );
 
